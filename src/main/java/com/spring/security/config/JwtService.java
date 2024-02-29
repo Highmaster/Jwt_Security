78c2d8f3ccec783ffe71815885bfee1c213aad37
Highmaster <nzechideraann@gmail.com>
2024-02-29 02:54:14 +0100

    Jwt authentication

---
+++ src/main/java/com/spring/security/config/JwtService.java
@@ -0,0 +1,40 @@
+package com.spring.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = " +Q4JbUTj4ZKxkCX2RNjFPGCcb7vG58PQjYU95d35AV0=\n";
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+
+    }
+
+    private Key getSignInKey() {
+        byte[] KeyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(KeyBytes);
+    }
+}
